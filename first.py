2cd1fa7f96f08f688f08a2daf8eea79e44c9b63f
levi <rh761209@gmail.com>
2017-03-28 02:31:49 +0800

    python test

---
+++ first.py
@@ -0,0 +1,8 @@
+#-*- coding: utf-8 -*-
+#-*- coding: cp950 -*-
+total = 1 + 99
+total2 = 100 - 99
+print "total =", total
+print "total3 =", total2
+print "total + total2 =", total + total2
+print "總和 =", total + total2
